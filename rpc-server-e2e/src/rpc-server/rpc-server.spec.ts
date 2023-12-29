ce86097fbcd7dde9e08ffcc423d96a2b296dfc5a
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-29 07:49:16 +0000

    Fix test

--- rpc-server-e2e/src/rpc-server/rpc-server.spec.ts
+++ rpc-server-e2e/src/rpc-server/rpc-server.spec.ts
@@ -1,8 +1,8 @@
-import grpc from '@grpc/grpc-js';
-import protoLoader from '@grpc/proto-loader';
+import * as grpc from '@grpc/grpc-js';
+import * as protoLoader from '@grpc/proto-loader';
 import { ProtoGrpcType } from './helloworld';
 
-const PROTO_PATH = __dirname + '../../../protos/helloworld.proto';
+const PROTO_PATH = __dirname + '/../../../protos/helloworld.proto';
 
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   keepCase: true,
@@ -17,14 +17,17 @@ const hello_proto = (
 ).helloworld;
 
 describe('CLI tests', () => {
-  it('should print a message', () => {
+  it('should print a message', (done) => {
     const client = new hello_proto.Greeter(
       'localhost:50051',
       grpc.credentials.createInsecure()
     );
 
-    client.sayHello({ name: 'world' }, function (err, response) {
-      expect(response?.message).toMatch(/Hello World/);
+    client.sayHello({ name: 'World' }, function (err, response) {
+      expect(err).toBeNull();
+      expect(response?.message).toEqual('Hello World');
+
+      done();
     });
   });
 });
