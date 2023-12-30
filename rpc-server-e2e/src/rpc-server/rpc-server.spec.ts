569ce5d4089c23d381fc3dfdd685ac983a9d36a2
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-30 10:01:16 +0000

    Refactor

--- rpc-server-e2e/src/rpc-server/rpc-server.spec.ts
+++ rpc-server-e2e/src/rpc-server/rpc-server.spec.ts
@@ -1,24 +1,9 @@
 import * as grpc from '@grpc/grpc-js';
-import * as protoLoader from '@grpc/proto-loader';
-import { ProtoGrpcType } from './helloworld';
-
-const PROTO_PATH = __dirname + '/../../../protos/helloworld.proto';
-
-const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
-});
-
-const helloProto = (
-  grpc.loadPackageDefinition(packageDefinition) as unknown as ProtoGrpcType
-).helloworld;
+import { helloworldPackageDefinition } from '@nodejs-grpc-experiment/helloworld-grpc-package-definition';
 
 describe('CLI tests', () => {
   it('should print a message', (done) => {
-    const client = new helloProto.Greeter(
+    const client = new helloworldPackageDefinition.Greeter(
       'localhost:50051',
       grpc.credentials.createInsecure()
     );
