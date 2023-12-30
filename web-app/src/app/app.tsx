7b48ed64461bbd0502c6237c654112348cacbef1
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-30 15:36:07 +0000

    Add sample

--- web-app/src/app/app.tsx
+++ web-app/src/app/app.tsx
@@ -1,8 +1,36 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import { useEffect, useState } from 'react';
+import {
+  GreeterClient,
+  HelloRequest,
+} from '@nodejs-grpc-experiment/rpc-web-client';
 
 export function App() {
-  return <div></div>;
+  const [state, setState] = useState('');
+  useEffect(() => {
+    const f = async () => {
+      const client = new GreeterClient(
+        'http://' + window.location.hostname + ':8080',
+        null,
+        null
+      );
+      const request = new HelloRequest();
+      request.setName('World');
+      client.sayHello(request, {}, (err, response) => {
+        if (err) {
+          setState(
+            `Unexpected error for sayHello: code = ${err.code}` +
+              `, message = "${err.message}"`
+          );
+        } else {
+          setState(response.getMessage());
+        }
+      });
+    };
+    f();
+  }, []);
+  return <div>{state}</div>;
 }
 
 export default App;
