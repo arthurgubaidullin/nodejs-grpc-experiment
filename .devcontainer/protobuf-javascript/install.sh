21bdcad4145f8a6c6e13029e11a07106dc30600d
Artur Gubaidullin <lapteuh@gmail.com>
2023-12-30 13:49:37 +0000

    Fix install.sh

--- .devcontainer/protobuf-javascript/install.sh
+++ .devcontainer/protobuf-javascript/install.sh
@@ -8,11 +8,15 @@ set -e
 # `ensure_nanolayer` is a bash function that will find any existing nanolayer installations,
 # and if missing - will download a temporary copy that automatically get deleted at the end
 # of the script
-ensure_nanolayer nanolayer_location "v0.5.5"
+ensure_nanolayer nanolayer_location "v0.5.4"
 
 
 $nanolayer_location \
     install \
     devcontainer-feature \
-    "ghcr.io/devcontainers-contrib/features/gh-release:1.0.24" \
-    --option repo='protocolbuffers/protobuf-javascript' --option assetRegex='.+\.tar\.gz' --option binaryNames='protoc-gen-js' --option version="$VERSION"
+    "ghcr.io/devcontainers-contrib/features/gh-release:1.0.23" \
+    --option repo='protocolbuffers/protobuf-javascript' --option binaryNames='protoc-gen-js' --option version="$VERSION" --option assetRegex='.+\.tar\.gz'
+
+
+
+echo 'Done!'
